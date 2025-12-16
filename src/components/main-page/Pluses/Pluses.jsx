5283be7100e9aa8e8cea6de8009053d08d219c32
dd <xeon3883@gmail.com>
2025-12-16 17:25:58 +0300

    кривой адаптив

--- src/components/main-page/Pluses/Pluses.jsx
+++ src/components/main-page/Pluses/Pluses.jsx
@@ -1,65 +1,68 @@
-import styles from "./styles.module.scss";
-import Image from "next/image";
+import Image from 'next/image'
+import styles from './styles.module.scss'
 
 export default function Pluses() {
-  return (
-    <section className="container">
-      <h2 className={styles.pluses_title}>ПЯТЬ ПЛЮСОВ ДЛЯ ВАШЕГО БИЗНЕСА!</h2>
-      <div className={styles.pluses_conatiner}>
-        <div className={styles.pluses_conatiner_left}>
-          <span>#1. Эффективные решения</span>
-          <p>
-            Вы хотели бы внедрить разграничение доступа пользователей?
-            Подключить удаленных сотрудников? Установить бесшовную WiFi-сеть,
-            офисную телефонию или видеонаблюдение?
-          </p>
-          <p>Поможем вам реализовать все это и многое другое!</p>
-          <Image
-            src="/images/pluses/image.png"
-            alt="pluses"
-            width={100}
-            height={200}
-          />
-        </div>
-        <div className={styles.pluses_conatiner_right}>
-          <div className={styles.pluses_conatiner_right_top}>
-            <div className={styles.pluses_conatiner_right_top__left}>
-              <span>#2. Предотвращение аварий</span>
-              <p>
-                Выполняя ежедневный мониторинг состояния оборудования, наши
-                специалисты выявят и ликвидируют потенциальные сбои до того, как
-                они повлияют на вашу работу.
-              </p>
-            </div>
-            <div className={styles.pluses_conatiner_right_top__right}>
-              <span>#3. Сокращение простоев</span>
-              <p>
-                Повысим продуктивность работы ваших сотрудников, предоставляя
-                консультации по ИТ-вопросам и оперативно устраняя неполадки.
-              </p>
-            </div>
-          </div>
-          <div className={styles.pluses_conatiner_right_bottom}>
-            <div className={styles.pluses_conatiner_right_bottom__left}>
-              <span>#4. Защита информации</span>
-              <p>
-                Защитим вашу информацию, файлы, базы 1С. Устраним риски утраты
-                данных, которые могут привести к финансовым и репутационным
-                потерям.
-              </p>
-            </div>
-            <div className={styles.pluses_conatiner_right_bottom__right}>
-              <span>#5. Содействие в развитии</span>
-              <p>
-                Необходима помощь ИТ-специалистов при переезде офиса, открытии
-                филиала, магазина, склада? Спланируем закупку оборудования,
-                подключение коммуникаций, монтаж сети и поможем оперативно
-                запуститься!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.pluses}>
+			<div className='container'>
+				<h2 className={styles.pluses_title}>ПЯТЬ ПЛЮСОВ ДЛЯ ВАШЕГО БИЗНЕСА!</h2>
+				<div className={styles.pluses_conatiner}>
+					<div className={styles.pluses_conatiner_left}>
+						<span>#1. Эффективные решения</span>
+						<p>
+							Вы хотели бы внедрить разграничение доступа пользователей?
+							Подключить удаленных сотрудников? Установить бесшовную WiFi-сеть,
+							офисную телефонию или видеонаблюдение?
+						</p>
+						<p>Поможем вам реализовать все это и многое другое!</p>
+
+						<Image
+							src='/images/pluses/image.png'
+							alt='pluses'
+							width={100}
+							height={200}
+						/>
+					</div>
+					<div className={styles.pluses_conatiner_right}>
+						<div className={styles.pluses_conatiner_right_top}>
+							<div className={styles.pluses_conatiner_right_top__left}>
+								<span>#2. Предотвращение аварий</span>
+								<p>
+									Выполняя ежедневный мониторинг состояния оборудования, наши
+									специалисты выявят и ликвидируют потенциальные сбои до того,
+									как они повлияют на вашу работу.
+								</p>
+							</div>
+							<div className={styles.pluses_conatiner_right_top__right}>
+								<span>#3. Сокращение простоев</span>
+								<p>
+									Повысим продуктивность работы ваших сотрудников, предоставляя
+									консультации по ИТ-вопросам и оперативно устраняя неполадки.
+								</p>
+							</div>
+						</div>
+						<div className={styles.pluses_conatiner_right_bottom}>
+							<div className={styles.pluses_conatiner_right_bottom__left}>
+								<span>#4. Защита информации</span>
+								<p>
+									Защитим вашу информацию, файлы, базы 1С. Устраним риски утраты
+									данных, которые могут привести к финансовым и репутационным
+									потерям.
+								</p>
+							</div>
+							<div className={styles.pluses_conatiner_right_bottom__right}>
+								<span>#5. Содействие в развитии</span>
+								<p>
+									Необходима помощь ИТ-специалистов при переезде офиса, открытии
+									филиала, магазина, склада? Спланируем закупку оборудования,
+									подключение коммуникаций, монтаж сети и поможем оперативно
+									запуститься!
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
--- src/components/main-page/Start/Start.jsx
+++ src/components/main-page/Start/Start.jsx
@@ -1,54 +1,56 @@
-import styles from "./styles.module.scss";
-import Image from "next/image";
+import Image from 'next/image'
+import styles from './styles.module.scss'
 
 export default function Start() {
-  return (
-    <section className="container">
-      <h2 className={styles.start_title}>ЭФФЕКТИВНЫЙ СТАРТ!</h2>
-      <div className={styles.start_container}>
-        <div className={styles.start_item}>
-          <span>Отчет о состоянии дел</span>
-          <p>
-            Проведем аудит и предоставим отчет о состоянии вашей компьютерной
-            инфраструктуры. Выявим проблемы, влияющие на работу вашей ИТ-системы
-            и безопасность данных.
-          </p>
-          <Image
-            src="/svg/start/first.svg"
-            alt="report"
-            width={75}
-            height={125}
-          />
-        </div>
-        <div className={styles.start_item}>
-          <span>План действий</span>
-          <p>
-            Составим и согласуем с вами дорожную карту отладки вашей
-            ИТ-инфраструктуры и решения накопившихся вопросов: как тех, с
-            которыми вы к нам обратились, так и выявленных в ходе аудита.
-          </p>
-          <Image
-            src="/svg/start/second.svg"
-            alt="report"
-            width={75}
-            height={125}
-          />
-        </div>
-        <div className={styles.start_item}>
-          <span>Техподдержка</span>
-          <p>
-            Ваши сотрудники получат доступ к горячей линии ИТ-поддержки. Мы
-            будем решать вопросы пользователей удаленно и на выездах, а вы
-            сможете сосредоточиться на ведении бизнеса!
-          </p>
-          <Image
-            src="/svg/start/three.svg"
-            alt="report"
-            width={75}
-            height={125}
-          />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.start}>
+			<div className='container'>
+				<h2 className={styles.start_title}>ЭФФЕКТИВНЫЙ СТАРТ!</h2>
+				<div className={styles.start_container}>
+					<div className={styles.start_item}>
+						<span>Отчет о состоянии дел</span>
+						<p>
+							Проведем аудит и предоставим отчет о состоянии вашей компьютерной
+							инфраструктуры. Выявим проблемы, влияющие на работу вашей
+							ИТ-системы и безопасность данных.
+						</p>
+						<Image
+							src='/svg/start/first.svg'
+							alt='report'
+							width={75}
+							height={125}
+						/>
+					</div>
+					<div className={styles.start_item}>
+						<span>План действий</span>
+						<p>
+							Составим и согласуем с вами дорожную карту отладки вашей
+							ИТ-инфраструктуры и решения накопившихся вопросов: как тех, с
+							которыми вы к нам обратились, так и выявленных в ходе аудита.
+						</p>
+						<Image
+							src='/svg/start/second.svg'
+							alt='report'
+							width={75}
+							height={125}
+						/>
+					</div>
+					<div className={styles.start_item}>
+						<span>Техподдержка</span>
+						<p>
+							Ваши сотрудники получат доступ к горячей линии ИТ-поддержки. Мы
+							будем решать вопросы пользователей удаленно и на выездах, а вы
+							сможете сосредоточиться на ведении бизнеса!
+						</p>
+						<Image
+							src='/svg/start/three.svg'
+							alt='report'
+							width={75}
+							height={125}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
