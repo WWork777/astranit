4cda1740630cc03add59770668e219c4e8e5dd23
id0ntc4re <parsikovevgenij470@gmail.com>
2025-12-17 10:46:37 +0700

    qwe

--- src/components/main-page/Saved-data/SavedData.jsx
+++ src/components/main-page/Saved-data/SavedData.jsx
@@ -1,8 +1,68 @@
 import styles from "./styles.module.scss";
+import Image from "next/image";
 
 export default function SavedData() {
   return (
     <section className={styles.saved_data}>
+      <div className={styles.mobile_saved__data}>
+        <div className={styles.mobile_title}>
+          <h2>
+            БЕСПОКОИТЕСЬ <br /> ЗА СОХРАННОСТЬ ДАННЫХ?
+          </h2>
+        </div>
+
+        <div className={styles.mobile_saved__data__image}>
+          <img
+            src="/images/saved/mobile.png"
+            alt="cheef"
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.mobile_checkbox_container}>
+          <span>Оставьте заявку на бесплатный ИТ-аудит!</span>
+          <div className={styles.mobile_checkbox}>
+            <div className={styles.mobile_checkbox_item}>
+              <Image
+                src="/images/saved/checkbox.svg"
+                alt="checkbox"
+                width={20}
+                height={20}
+              />
+              <span>проверим ИТ-инфраструктуру;</span>
+            </div>
+            <div className={styles.mobile_checkbox_item}>
+              <Image
+                src="/images/saved/checkbox.svg"
+                alt="checkbox"
+                width={20}
+                height={20}
+              />
+              <span>выявим слабые места;</span>
+            </div>
+            <div className={styles.mobile_checkbox_item}>
+              <Image
+                src="/images/saved/checkbox.svg"
+                alt="checkbox"
+                width={20}
+                height={20}
+              />
+              <span>предоставим отчет о состоянии дел;</span>
+            </div>
+            <div className={styles.mobile_checkbox_item}>
+              <Image
+                src="/images/saved/checkbox.svg"
+                alt="checkbox"
+                width={20}
+                height={20}
+              />
+              <span>спланируем меры по исключению рисков.</span>
+            </div>
+          </div>
+          <button className={styles.button_mobile}>
+            <span>Получить аудит!</span>
+          </button>
+        </div>
+      </div>
       <div className={styles.clipped}>
         <div className={styles.clipped_left}>
           <img
