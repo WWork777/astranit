698aca91c919c58534ede93e04018c322a799e1b
dd <xeon3883@gmail.com>
2025-12-23 22:35:18 +0300

    старт адаптив

--- src/components/main-page/Start/Start.jsx
+++ src/components/main-page/Start/Start.jsx
@@ -7,138 +7,80 @@ export default function Start() {
 			<div className={styles.container}>
 				<h2 className={styles.start_title}>ЭФФЕКТИВНЫЙ СТАРТ!</h2>
 				<div className={styles.start_container}>
-					<Image
-						src='/svg/start/arrow.svg'
-						alt='arrow'
-						width={500}
-						height={100}
-						className={styles.arrow}
-					/>
-					<div className={styles.start_item}>
-						<div className={styles.item_content}>
-							<div className={styles.text_wrapper}>
-								<span className={styles.item_title}>Отчет о состоянии дел</span>
-								<p className={styles.item_description}>
-									Проведем аудит и предоставим отчет о состоянии вашей
-									компьютерной инфраструктуры. Выявим проблемы, влияющие на
-									работу вашей ИТ-системы и безопасность данных.
-								</p>
-							</div>
-							<div className={styles.image_wrapper}>
-								<Image
-									src='/svg/start/first.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-							</div>
-							<div className={styles.text_wrapper_mobile}>
-								<Image
-									src='/svg/start/first.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-								<span className={styles.item_title}>Отчет о состоянии дел</span>
-							</div>
-							<div className={styles.image_wrapper_mobile}>
-								<p className={styles.item_description}>
-									Проведем аудит и предоставим отчет о состоянии вашей
-									компьютерной инфраструктуры. Выявим проблемы, влияющие на
-									работу вашей ИТ-системы и безопасность данных.
-								</p>
+					{/* Первый блок */}
+					<div className={styles.start_item_wrapper}>
+						<div className={styles.start_item}>
+							<div className={styles.item_content}>
+								<div className={styles.text_wrapper}>
+									<span className={styles.item_title}>
+										Отчет о состоянии дел
+									</span>
+									<p className={styles.item_description}>
+										Проведем аудит и предоставим отчет о состоянии вашей
+										компьютерной инфраструктуры. Выявим проблемы, влияющие на
+										работу вашей ИТ-системы и безопасность данных.
+									</p>
+								</div>
+								<div className={styles.image_wrapper}>
+									<Image
+										src='/svg/start/first.svg'
+										alt='Отчет о состоянии дел'
+										width={75}
+										height={125}
+										className={styles.item_image}
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
-					<Image
-						src='/svg/start/arrow.svg'
-						alt='arrow'
-						width={500}
-						height={100}
-						className={styles.arrow}
-					/>
-					<div className={styles.start_item}>
-						<div className={styles.item_content}>
-							<div className={styles.text_wrapper}>
-								<span className={styles.item_title}>План действий</span>
-								<p className={styles.item_description}>
-									Составим и согласуем с вами дорожную карту отладки вашей
-									ИТ-инфраструктуры и решения накопившихся вопросов: как тех, с
-									которыми вы к нам обратились, так и выявленных в ходе аудита.
-								</p>
-							</div>
-							<div className={styles.image_wrapper}>
-								<Image
-									src='/svg/start/second.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-							</div>
-							<div className={styles.text_wrapper_mobile}>
-								<Image
-									src='/svg/start/second.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-								<span className={styles.item_title}>План действий</span>
-							</div>
-							<div className={styles.image_wrapper_mobile}>
-								<p className={styles.item_description}>
-									Составим и согласуем с вами дорожную карту отладки вашей
-									ИТ-инфраструктуры и решения накопившихся вопросов: как тех, с
-									которыми вы к нам обратились, так и выявленных в ходе аудита.
-								</p>
+
+					{/* Второй блок */}
+					<div className={styles.start_item_wrapper}>
+						<div className={styles.start_item}>
+							<div className={styles.item_content}>
+								<div className={styles.text_wrapper}>
+									<span className={styles.item_title}>План действий</span>
+									<p className={styles.item_description}>
+										Составим и согласуем с вами дорожную карту отладки вашей
+										ИТ-инфраструктуры и решения накопившихся вопросов: как тех,
+										с которыми вы к нам обратились, так и выявленных в ходе
+										аудита.
+									</p>
+								</div>
+								<div className={styles.image_wrapper}>
+									<Image
+										src='/svg/start/second.svg'
+										alt='План действий'
+										width={75}
+										height={125}
+										className={styles.item_image}
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
-					<Image
-						src='/svg/start/arrow.svg'
-						alt='arrow'
-						width={500}
-						height={100}
-						className={styles.arrow}
-					/>
-					<div className={styles.start_item}>
-						<div className={styles.item_content}>
-							<div className={styles.text_wrapper}>
-								<span className={styles.item_title}>Техподдержка</span>
-								<p className={styles.item_description}>
-									Ваши сотрудники получат доступ к горячей линии ИТ-поддержки.
-									Мы будем решать вопросы пользователей удаленно и на выездах, а
-									вы сможете сосредоточиться на ведении бизнеса!
-								</p>
-							</div>
-							<div className={styles.image_wrapper}>
-								<Image
-									src='/svg/start/three.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-							</div>
-							<div className={styles.text_wrapper_mobile}>
-								<Image
-									src='/svg/start/three.svg'
-									alt='report'
-									width={75}
-									height={125}
-									className={styles.item_image}
-								/>
-								<span className={styles.item_title}>Техподдержка</span>
-							</div>
-							<div className={styles.image_wrapper_mobile}>
-								<p className={styles.item_description}>
-									Ваши сотрудники получат доступ к горячей линии ИТ-поддержки.
-									Мы будем решать вопросы пользователей удаленно и на выездах, а
-									вы сможете сосредоточиться на ведении бизнеса!
-								</p>
+
+					{/* Третий блок*/}
+					<div className={styles.start_item_wrapper}>
+						<div className={styles.start_item}>
+							<div className={styles.item_content}>
+								<div className={styles.text_wrapper}>
+									<span className={styles.item_title}>Техподдержка</span>
+									<p className={styles.item_description}>
+										Ваши сотрудники получат доступ к горячей линии ИТ-поддержки.
+										Мы будем решать вопросы пользователей удаленно и на выездах,
+										а вы сможете сосредоточиться на ведении бизнеса!
+									</p>
+								</div>
+								<div className={styles.image_wrapper}>
+									<Image
+										src='/svg/start/three.svg'
+										alt='Техподдержка'
+										width={75}
+										height={125}
+										className={styles.item_image}
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
