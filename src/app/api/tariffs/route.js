9d32a8badb81fde8ffd3c2a816220fd28b915bf8
dd <xeon3883@gmail.com>
2026-01-03 01:52:08 +0300

    правки

--- src/app/api/tariffs/route.js
+++ src/app/api/tariffs/route.js
@@ -2,7 +2,7 @@ import db from '@/lib/db'
 
 export async function GET(req) {
 	try {
-		const [rows] = await db.query('SELECT * FROM Terea')
+		const [rows] = await db.query('SELECT * FROM Tariffs')
 		return new Response(JSON.stringify(rows), {
 			status: 200,
 			headers: { 'Content-Type': 'application/json' },
