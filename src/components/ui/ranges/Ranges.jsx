c97e7ca6a3fad8cb8117c2989d060d5e73bf1e6a
dd <xeon3883@gmail.com>
2026-01-04 12:34:07 +0300

    тарифы слайдер

--- src/components/ui/ranges/Ranges.jsx
+++ src/components/ui/ranges/Ranges.jsx
@@ -199,9 +199,124 @@ const Ranges = observer(({ styles }) => {
 	const pathname = usePathname()
 	return (
 		<>
-			<ul className={styles.ranges}>
-				{/* Компьютеры */}
-				<div className={styles.ranges_mobile_left}>
+			{pathname === '/' ? (
+				<ul className={styles.ranges}>
+					{/* Компьютеры */}
+					<div className={styles.ranges_mobile_top}>
+						<li className={styles.ranges_box1}>
+							<h2 className={styles.ranges_box1_title}>Компьютеры</h2>
+							<div className={styles.ranges_box1_body}>
+								<div className={styles.range_button}>
+									<GoChevronLeft
+										onClick={sliderStore.decrementComputer}
+										style={{ cursor: 'pointer' }}
+									/>
+									{sliderStore.computerValue}
+									<GoChevronRight
+										onClick={sliderStore.incrementComputer}
+										style={{ cursor: 'pointer' }}
+									/>
+								</div>
+								<input
+									type='range'
+									min={sliderStore.sliderConfig.min}
+									max={sliderStore.sliderConfig.max}
+									step={sliderStore.sliderConfig.step}
+									value={sliderStore.computerValue}
+									onChange={e =>
+										sliderStore.setComputerValue(parseInt(e.target.value))
+									}
+									className={styles.range_input}
+									style={{
+										background: sliderStore.getRangeBackground(
+											sliderStore.computerValue
+										),
+									}}
+								/>
+							</div>
+						</li>
+
+						{/* Серверы */}
+						<li className={styles.ranges_box2}>
+							<h2 className={styles.ranges_box1_title}>Серверы</h2>
+							<div className={styles.ranges_box1_body}>
+								<div className={styles.range_button}>
+									<GoChevronLeft
+										onClick={sliderStore.decrementServers}
+										style={{ cursor: 'pointer' }}
+									/>
+									{sliderStore.serversValue}
+									<GoChevronRight
+										onClick={sliderStore.incrementServers}
+										style={{ cursor: 'pointer' }}
+									/>
+								</div>
+								<input
+									type='range'
+									min={sliderStore.sliderConfig.min}
+									max={sliderStore.sliderConfig.max}
+									step={sliderStore.sliderConfig.step}
+									value={sliderStore.serversValue}
+									onChange={e =>
+										sliderStore.setServersValue(parseInt(e.target.value))
+									}
+									className={styles.range_input}
+									style={{
+										background: sliderStore.getRangeBackground(
+											sliderStore.serversValue
+										),
+									}}
+								/>
+							</div>
+						</li>
+					</div>
+
+					<div className={styles.ranges_mobile_bottom}>
+						{/* Офисы */}
+						<li className={styles.ranges_box3}>
+							<h2 className={styles.ranges_box1_title}>
+								Офисы / магазины / склады /...
+							</h2>
+							<div className={styles.ranges_box1_body}>
+								<div className={styles.range_button}>
+									<GoChevronLeft
+										onClick={sliderStore.decrementOffices}
+										style={{ cursor: 'pointer' }}
+									/>
+									{sliderStore.officesValue}
+									<GoChevronRight
+										onClick={sliderStore.incrementOffices}
+										style={{ cursor: 'pointer' }}
+									/>
+								</div>
+								<input
+									type='range'
+									min={sliderStore.sliderConfig.min}
+									max={sliderStore.sliderConfig.max}
+									step={sliderStore.sliderConfig.step}
+									value={sliderStore.officesValue}
+									onChange={e =>
+										sliderStore.setOfficesValue(parseInt(e.target.value))
+									}
+									className={styles.range_input}
+									style={{
+										background: sliderStore.getRangeBackground(
+											sliderStore.officesValue
+										),
+									}}
+								/>
+							</div>
+						</li>
+						<li>
+							<Link href={'/tariffs'}>
+								<button>Расчитать</button>
+							</Link>
+						</li>
+					</div>
+				</ul>
+			) : (
+				<ul className={styles.ranges}>
+					{/* Компьютеры */}
 					<li className={styles.ranges_box1}>
 						<h2 className={styles.ranges_box1_title}>Компьютеры</h2>
 						<div className={styles.ranges_box1_body}>
@@ -268,11 +383,8 @@ const Ranges = observer(({ styles }) => {
 							/>
 						</div>
 					</li>
-				</div>
 
-				{/* Офисы */}
-				<div className={styles.ranges_mobile_right}>
-					{' '}
+					{/* Офисы */}
 					<li className={styles.ranges_box3}>
 						<h2 className={styles.ranges_box1_title}>
 							Офисы / магазины / склады /...
@@ -307,15 +419,8 @@ const Ranges = observer(({ styles }) => {
 							/>
 						</div>
 					</li>
-					{pathname === '/' ? (
-						<Link href={'/tariffs'}>
-							<button className={styles.button_mobile_mod}>
-								<span>Рассчитать!</span>
-							</button>
-						</Link>
-					) : null}
-				</div>
-			</ul>
+				</ul>
+			)}
 		</>
 	)
 })
